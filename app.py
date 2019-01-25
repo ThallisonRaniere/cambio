a7f32a40c7ec0d8f593ffee3cd99f2a9d84fa93f
ThallisonRaniere <thallisonraniere@gmail.com>
2019-01-25 00:47:39 -0200

    Remover debug

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def cambio(fr, to):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
